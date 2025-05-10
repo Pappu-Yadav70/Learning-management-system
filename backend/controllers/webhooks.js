c8d8169b7f9e22c8789c45f3b58e818331256355
pappu-yadav70 <technoyadav1234@gmail.com>
2025-05-10 22:17:59 +0530

    first commit

---
+++ backend/controllers/webhooks.js
@@ -0,0 +1,59 @@
+import { Webhook } from "svix";
+import User from "../models/User";
+
+
+
+// API CONTROLLER FUNCTION TO MANAGE CLERK USER WITH DATABSE
+
+export const clerkwebhooks = async(req,res)=>{
+    try {
+        const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        
+        await  webhook.verify(JSON.stringify(req.body),{
+            "svix-id":req.headers['svix-id'],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        })
+        //we need data from request body
+        const {data,type} = req.body
+        switch (type) {
+            case 'user.created':{
+                const userData = {
+                    _id: data.id,
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    image_url: data.image_url,
+
+                }
+                await User.create(userData) // it will store the data on mongodb database
+                res.json({})
+                break;
+
+            }
+                // user updation on database
+            case 'user.updated':{
+                 const userData = {
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    image_url: data.image_url,
+
+                }
+                await User.findByIdAndUpdate(data.id,userData) //USED finbyuserdata method because we updating the user data
+                res.json({})
+                break
+            }
+             // user deletion on databse
+            case 'user.deleted':{
+               await User.findByIdAndDelete(data.id)
+               res.json({})
+               break;
+            }
+            default:
+                break;
+        }
+
+    } catch (error) {
+        res.json({success:false, message: error.message})
+        
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import { clerkwebhooks } from './controllers/webhooks.js';
+
+
+// intialize  express
+
+const app = express();
+
+
+// connect to databse
+await connectDB()
+
+
+// middleware
+app.use(cors())  //using cors becz we can  connect our backend other domain
+
+// Routes
+app.get('/', (req,res)=> res.send("API WORKING"))
+app.post('/clerk', express.json(), clerkwebhooks)
+
+
+
+// port
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
